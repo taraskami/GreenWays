43e400857ed109e7714794005b9ae9441619ab9f
gwang111 <garyawang1@gmail.com>
2020-11-07 14:16:16 -0800

    website so far

---
+++ webpage/map.js
@@ -0,0 +1,10 @@
+// Initialize and add the map
+function initMap() {
+// The location of Uluru
+const uluru = { lat: 42.7345, lng: -73.675 };
+// The map, centered at Uluru
+const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+});
+}
