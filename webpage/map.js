6f7ba114bd77e233ae718409d714f189486ee876
gwang111 <garyawang1@gmail.com>
2020-11-07 16:40:19 -0800

    geolocator

--- webpage/map.js
+++ webpage/map.js
@@ -1,9 +1,27 @@
 // Initialize and add the map
+var geocoder;
+var map;
+
 function initMap() {
- 
-const troy_kitchen = { lat: 42.728802, lng: -73.690267};
-const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
-    center: troy_kitchen,
-});
+    geocoder = new google.maps.Geocoder();
+    const troy_kitchen = { lat: 42.728802, lng: -73.690267};
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: troy_kitchen,
+    });
 }
+
+function changeLoc() {
+    var address = document.getElementById("location").value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
