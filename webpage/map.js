209132ed21fd5fa611a934704b0173679d2024b1
Karthik Imayavaramban <scorpiokarth@gmail.com>
2020-11-08 06:20:09 -0500

    xml support added
    uses jquerty and here api to grab real time traffic data and parse as an xml file. uses geodata from google maps to automatically generate new xml when user searches area

--- webpage/map.js
+++ webpage/map.js
@@ -2,14 +2,42 @@
 var geocoder;
 var map;
 
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+
 function initMap() {
+
     geocoder = new google.maps.Geocoder();
     const troy_kitchen = { lat: 42.728802, lng: -73.690267};
     map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
-        center: troy_kitchen,
+        center: troy_kitchen
     });
+    
+    fetch('https://traffic.ls.hereapi.com/traffic/6.2/flow.xml?apiKey=VVjp8IvlTyyyS8tk038Dm0NYoDaI_dlcSxlMNpbhKUE&prox=42.728412,-73.691785,5000&responseattributes=sh')
+    .then(response => response.text())
+    .then(xmlString => $.parseXML(xmlString))
+    .then((out) => {
+        console.log('Output: ', out);
+        const RW = out.getElementsByTagName("RW");
+        console.log(RW.length);
+    }).catch(err => console.error(err));
 }
+
+
 
 function changeLoc() {
     var address = document.getElementById("location").value;
@@ -20,6 +48,16 @@ function changeLoc() {
             map: map,
             position: results[0].geometry.location
         });
+        var lat = results[0].geometry.location.lat();
+        var lng = results[0].geometry.location.lng();
+        fetch('https://traffic.ls.hereapi.com/traffic/6.2/flow.xml?apiKey=VVjp8IvlTyyyS8tk038Dm0NYoDaI_dlcSxlMNpbhKUE&prox=' + lat + ','+lng +',5000&responseattributes=sh')
+        .then(response => response.text())
+        .then(xmlString => $.parseXML(xmlString))
+        .then((out) => {
+            console.log('Output: ', out);
+            const RW = out.getElementsByTagName("RW");
+            console.log(RW.length);
+        }).catch(err => console.error(err));
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
