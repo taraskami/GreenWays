92a8acc792573bbf2709b727ee7c062189840331
gwang111 <garyawang1@gmail.com>
2020-11-07 16:58:04 -0800

    button but not on enter

--- webpage/map.js
+++ webpage/map.js
@@ -24,4 +24,15 @@ function changeLoc() {
         alert('Geocode was not successful for the following reason: ' + status);
       }
     });
-  }
+}
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("onEnter", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("goto").click();
+    }
+});
